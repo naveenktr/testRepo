8d1b747801409bbebebe3aa8d85dc8dc07697564
Naveen KK <ktrnaveen91@gmail.com>
2023-03-22 21:25:29 +0530

    Removed unused css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./Assets/theme";
 import Layout from "./Components/Shared/Layout/Layout";
-import "./index.css";
 
 export default function App() {
   return (
